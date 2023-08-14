2f69fdd984cf70c4933933193f04642419ca3721
krizcold <krizcold@gmail.com>
2023-08-14 09:42:19 -0400

    Updated UI

--- src/components/Exercice.js
+++ src/components/Exercice.js
@@ -8,9 +8,10 @@ import '@mozilla/lilypad-ui/dist/styles/theme.scss'; // Import the Lilypad CSS
 export default function ExerciceField({UnitId, uuid}){
     const [exercice, setExercice] = useState(undefined);
     const [translationCode, setTranslationCode] = useState(navigator.language.split('-')[0]);
+    const [selectedChoice, setSelectedChoice] = useState(null);
 
-    async function callAPI(currentUnitId, currentUUID = undefined, fake = true) {
-
+    async function callAPI(currentUnitId, currentUUID = undefined, fake = false) {
+        
         if(fake){
             await new Promise(r => setTimeout(r, 1000));
             return {
@@ -48,36 +49,70 @@ export default function ExerciceField({UnitId, uuid}){
         const headers = new Headers();
         headers.append("x-request-to", "DKDNidkwinDKSNdiwnd")
         //13.209.103.192:9901   ---   https://api-service.solve-mate.com/"
-        const baseUrl = "http://api-service.solve-mate.com/"
-        const createPostPath = `v1/api/solve/sample/${currentUnitId}`
-        const recallGetPath = `v2/api/solve/sample/${currentUUID}`
+        const baseUrl = "https://api-service.solve-mate.com/"
+        const createPostPath = `v1/api/solve/sample/${currentUnitId}` //Short val
+        const recallGetPath = `v2/api/solve/sample/${currentUUID}` //Long val
         let query, method
         if(currentUUID === undefined){
             query = new URL(baseUrl+ createPostPath);
             method = "POST"
+            console.log("Post API")
         }
-        else{
+        else {
             query = new URL(baseUrl+ recallGetPath);
             method = "GET"
+            console.log("Get API")
         }
         query.searchParams.append("translationCode", translationCode);
-        const response = await fetch(query, {method, headers, mode: 'no-cors'});
+        const response = await fetch(query, {method, headers});
         const data = await response.json();
         return data;
+    
     }
-
+    var register_uuid = undefined;
+    var ignoreAPI = true; //the api is called twice (for some reason) and we are running out of time c:
     function initExercice(){
+        if (ignoreAPI === false) {ignoreAPI = true; return;} else {ignoreAPI = false;}
         if(exercice !== undefined) return;
+
+        console.log("<-----(UnitId: "+UnitId+") (uuid:"+uuid+")----->")
         callAPI(UnitId, uuid).then((data) => {
             console.log(data)
             setExercice(data)
+            console.log("QUESTION uuid get: "+data.uuid)
+            register_uuid = data.uuid;
         })
     }
 
-    const handleClick = () => {
+    const handleClick = (choice, index, buttonid, _uuid) => {
         // Perform desired action here!
+        setSelectedChoice(choice);
+        console.log("Option Clic");
+        
+
+        console.log("Button._uuid:"+_uuid);
+
+        callAPI(undefined, _uuid).then((data) => {
+            console.log(data)
+            console.log("RESPONSE get: "+data.uuid)
+        })
+        
+        //Set pressed button attribute to Correct/Incorrect for the CSS
+        
+        const button = document.getElementById(buttonid);
+        
+        if (true) //(??? is correct)
+        {
+            button.setAttribute("mymode", "Correct");
+        }
+        else
+        {
+            button.setAttribute("mymode", "Incorrect");
+        }
     }
-    initExercice()
+
+    
+    React.useEffect(()=>{initExercice()},[])
     return (
         <>
             {exercice === undefined ? <p>Loading..</p> :
@@ -87,26 +122,37 @@ export default function ExerciceField({UnitId, uuid}){
                         This section contains the PROBLEM and the EXPLAINATION
                     */
                 >
-                    <p><h3><Latex displayMode={true}>{exercice.problem}</Latex></h3></p>
+                    <h3><Latex displayMode={true}>{exercice.problem}</Latex></h3>
                     <p><Latex displayMode={true}>{exercice.explanation}</Latex></p>
                 </div>
             }
-
+            
             <ul className="choiseList">
                 {exercice && exercice.choiceList.map((choice, index) =>
                     /*
                         This section contains the ANSWER BUTTONS
-                        (Note: I separate the script to have the answers outside the main BOX)
+                        (Note: I separated the script to have the answers outside the main BOX)
                     */
-                    <li key={index}>
-                        <Button
+                   
+                    <li key={index} className="Buttons">
+                        
+                        <Button 
+                            /*
+                                The id is being used to determinate how the selected
+                                answer should look through CSS on the index.css file
+                                id={"aButton-"+UnitId+"-"+index}
+                                should end up looking like:
+                                aButton-126-6
+                            */
+                            //category={choice===answer?"secondary_solid":"primary_solid"}
                             category="primary_solid"
-                            onClick={function handleClick(choice, index) { }}
-                            //text={<Latex displayMode={true}>{choice}</Latex>} //Original
+                            id={"aButton-"+UnitId+"-"+index}
+                            //classProp={exercice.uuid}
+                            onClick={() => handleClick(choice,index,"aButton-"+UnitId+"-"+index,exercice.uuid)}
                             text={<Latex displayMode={false}>{String.fromCharCode(65 + index)+ ". " + choice}</Latex>} //+Alphabet Value
                             type="button"
                             size="large"
-                            
+                            disabled={selectedChoice !== null} //Disable all buttons once an option is selected
                         />
                     </li>
                 )}
--- src/components/Procyan.js
+++ src/components/Procyan.js
@@ -1,36 +1,57 @@
 import ExerciceForm from "./ExerciceForm";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ExerciceField from "./Exercice";
 
 /*
-    1. Create a form with a select input to choose the type of classes
-    2. Call the api and render an exercice
+    [Done]-1. Create a form with a select input to choose the type of classes
+    [Done]-2. Call the api and render an exercice
     3. Let the user respond correctly or not and show the result
     4. Let the user change language
     5. Let the user ask for a new exercice in a different type of classes
 */
 
 export default function Procyan() {
+    
     let defaultUnitId = 126;
     //const [translationCode, setTranslationCode] = useState(navigator.language.split('-')[0]);
     const [exerciceList, setExerciceList] = useState([]);
 
-    const listItems = exerciceList.map(exercice =>
-        <li key={exercice.id}>
-           <ExerciceField uuid={exercice.unitId}/>
+    const listItems = exerciceList.map(exercice => {
+        //console.warn(exercice);
+        return <li key={exercice.unitId}>
+           <ExerciceField UnitId={exercice.unitId}/>
         </li>
-    );
+    });
 
     function addExercice(unitId) {
+        console.log("---[Add an exercice!]---")
         setExerciceList([...exerciceList, {unitId: unitId, id: exerciceList.length}])
+        /*setExerciceList(()=>{
+            return [...exerciceList, {unitId: unitId, id: exerciceList.length}];
+        })*/
     }
+    
 
+    useEffect(()=>{
+        const urlParams = new URLSearchParams(window.location.search);
+        const unitId = urlParams.get("unitId");
+        
+        if (unitId)
+        {
+            addExercice(unitId);
+        }
+    
+    }, []);
+
+    //<li> is the Dropdown list
+    //{listItems} is the resulting Content
     return (
         <>
             <ul>
-                {listItems}
                 <li><ExerciceForm unitId={exerciceList[exerciceList.length-1]?.unitId || defaultUnitId} onUpdateUnitId={addExercice} />
                 </li>
+
+                {listItems}
             </ul>
         </>
     );
